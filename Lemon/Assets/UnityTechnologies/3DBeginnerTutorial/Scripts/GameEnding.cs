95b7b134cb98d667237fab0d116006b3417273a7
Ziobots <zionbots@gmail.com>
2024-11-23 04:33:16 -0500

    gargoyle

--- Lemon/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
+++ Lemon/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -1,34 +1,46 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UnityTechnologies._3DBeginnerTutorial.Scripts {
 	public class GameEnding : MonoBehaviour {
 		public float fadeDuration = 1f;
 		public float displayImageDuration = 1f;
 		public GameObject player;
-		public CanvasGroup exitBackgroundImageCanvasGroup;
+		public CanvasGroup exitBackgroundImageCanvasGroup; 
+		public CanvasGroup caughtBackgroundImageCanvasGroup;
 
-		bool m_IsPlayerAtExit;
-		float m_Timer;
+		private bool m_IsPlayerAtExit;
+		private bool m_IsPlayerCaught;
+		private float m_Timer;
 
-		void OnTriggerEnter (Collider other) {
+		private void OnTriggerEnter (Collider other) {
 			if (other.gameObject == player) {
 				m_IsPlayerAtExit = true;
 			}
 		}
 
-		void Update () {
-			if(m_IsPlayerAtExit) {
-				EndLevel ();
+		public void CaughtPlayer () {
+			m_IsPlayerCaught = true;
+		}
+
+		private void Update () {
+			if (m_IsPlayerAtExit) {
+				EndLevel(exitBackgroundImageCanvasGroup, false);
+			}else if (m_IsPlayerCaught) {
+				EndLevel(caughtBackgroundImageCanvasGroup, true);
 			}
 		}
 
-		void EndLevel () {
+		private void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart) {
 			m_Timer += Time.deltaTime;
+			imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-			exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-
-			if(m_Timer > fadeDuration + displayImageDuration) {
-				Application.Quit ();
+			if (m_Timer > fadeDuration + displayImageDuration) {
+				if (doRestart) {
+					SceneManager.LoadScene(0);
+				}else {
+					Application.Quit();
+				}
 			}
 		}
 	}
