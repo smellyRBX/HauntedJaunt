b63b8a1a48613160d9af21f199574d2a65b2c479
Ziobots <zionbots@gmail.com>
2024-11-23 03:28:43 -0500

    ending

--- Lemon/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
+++ Lemon/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameEnding : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace UnityTechnologies._3DBeginnerTutorial.Scripts {
+	public class GameEnding : MonoBehaviour {
+		public float fadeDuration = 1f;
+		public float displayImageDuration = 1f;
+		public GameObject player;
+		public CanvasGroup exitBackgroundImageCanvasGroup;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		bool m_IsPlayerAtExit;
+		float m_Timer;
+
+		void OnTriggerEnter (Collider other) {
+			if (other.gameObject == player) {
+				m_IsPlayerAtExit = true;
+			}
+		}
+
+		void Update () {
+			if(m_IsPlayerAtExit) {
+				EndLevel ();
+			}
+		}
+
+		void EndLevel () {
+			m_Timer += Time.deltaTime;
+
+			exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+			if(m_Timer > fadeDuration + displayImageDuration) {
+				Application.Quit ();
+			}
+		}
+	}
 }
